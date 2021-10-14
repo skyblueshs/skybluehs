fb5aeb03f54243a32603baa9e9d9c076ccaee29f
skyblueshs <jiin4ever@naver.com>
2021-10-14 21:06:47 +0900

    수정

--- js/typinganimation.js
+++ js/typinganimation.js
@@ -21,7 +21,7 @@ TxtRotate.prototype.tick = function () {
   this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
 
   var that = this;
-  var delta = 250 - Math.random() * 100;
+  var delta = 130 - Math.random() * 100;
 
   if (this.isDeleting) { delta /= 5; }
 
@@ -31,7 +31,7 @@ TxtRotate.prototype.tick = function () {
   } else if (this.isDeleting && this.txt === '') {
       this.isDeleting = false;
       this.loopNum++;
-      delta = 500;
+      delta = 300;
   }
 
   setTimeout(function () {
